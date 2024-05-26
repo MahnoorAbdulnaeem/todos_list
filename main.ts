b3f0c520f8169d9cb752a349c7fafdcb053b8b06
Mahnoor <160059819+MahnoorAbdulnaeem@users.noreply.github.com>
2024-05-26 07:49:22 -0700

    first Todos list

---
+++ main.ts
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let todos = []
+
+let condition = true;
+
+while(condition)
+
+{ 
+    let todosQuestions = await inquirer.prompt(
+
+[  
+        {
+        name:"firstQuestion",
+        type:"input",
+        message:"what would like to add in you todos?",
+        },
+        {
+
+    name:"secondQuestion",
+    type:"confirm",
+    message:"would you like to add  more in your todos?",
+    default:"true",
+      },
+]
+);
+todos.push(todosQuestions.firstQuestion);
+console.log(todos)
+// The loop is running on the based of this variable condition
+condition = todosQuestions.secondQuestion;
+
+};
